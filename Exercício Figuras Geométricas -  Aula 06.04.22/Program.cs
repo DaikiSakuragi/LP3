4b5f672c32202cf610a295ba08a5ade8fd51c322
DaikiSakuragi <daikisakuragi@outlook.com>
2022-04-22 16:10:03 -0300

    Exercício Quadrado e Retângulo - Aula 06/04/22

---
+++ Exercício Figuras Geométricas -  Aula 06.04.22/Program.cs
@@ -0,0 +1,14 @@
+﻿Quadrado q1 = new Quadrado(20.0);
+Console.WriteLine(q1.Lado);
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Console.WriteLine("--------------------");
+
+Retangulo r1 = new Retangulo(3.0, 9.0);
+Console.WriteLine(r1.Base);
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro);
+
+
